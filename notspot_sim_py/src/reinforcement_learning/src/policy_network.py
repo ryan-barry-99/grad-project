7a671a64268b71836be3880210ad5ad185522c45
Ryan Barry <ryanbarrywheels@gmail.com>
2024-04-04 17:09:16 -0400

    Add ability to track rewards

--- notspot_sim_py/src/reinforcement_learning/src/policy_network.py
+++ notspot_sim_py/src/reinforcement_learning/src/policy_network.py
@@ -4,8 +4,16 @@ import rospy
 
 class PolicyNetwork:
     def __init__(self):
-        pass
+        rospy.init_node('policy_network_node', anonymous=True)
+        # Load hyperparameters from YAML file
+        self.hyperparameters = rospy.get_param('/policy_network/hyperparameters', default={})[0]
+        if self.hyperparameters['new_run']:
+            rospy.set_param('/RL/runs/new_run', True)
+        else:
+            rospy.set_param('/RL/runs/new_run', False)
+
+        rospy.spin()
 
 
 if __name__ == '__main__':
-    model = PolicyNetwork()
+    model = PolicyNetwork() 
--- notspot_sim_py/src/reinforcement_learning/src/reward.py
+++ notspot_sim_py/src/reinforcement_learning/src/reward.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 
 import rospy
+import rospkg
 from geometry_msgs.msg import PoseStamped
 from reinforcement_learning.msg import Heuristic
 from std_msgs.msg import Float32, Bool
+import os
+from datetime import datetime
 
 class Reward:
     def __init__(self):
         rospy.init_node('reward_node', anonymous=True)
         
         rospy.Subscriber('/gazebo/model_poses/robot/notspot', PoseStamped, self.robot_pose_callback)
-        rospy.Subscriber('/heuristic/goal/closest', Heuristic, self.heuristic_callback)
-        rospy.Subscriber('/episode/new', Bool, self.new_episode_callback)
+        rospy.Subscriber('/RL/heuristic/goal/closest', Heuristic, self.heuristic_callback)
+        rospy.Subscriber('/RL/episode/new', Bool, self.new_episode_callback)
+        
+        self.new_episode_pub = rospy.Publisher('/RL/episode/new', Bool, queue_size=10)
         self.robot_pose_stamped = PoseStamped()
         self.publishers = {
-            'action_reward': rospy.Publisher('reward/action', Float32, queue_size=10),
-            'total_reward': rospy.Publisher('reward/total', Float32, queue_size=10)
+            'action_reward': rospy.Publisher('/RL/reward/action', Float32, queue_size=10),
+            'total_reward': rospy.Publisher('/RL/reward/total', Float32, queue_size=10)
         }
+        self.rospack = rospkg.RosPack()
+        self.rewards_folder = self.rospack.get_path('reinforcement_learning') + '/runs'
+
         self.closest_heuristic = Heuristic()
         self.total_reward = 0
+        self.init_rewards_dir = False
         self.episode_num = 0
+        self.set_rewards_folder()
+        self.new_episode = False
+
         self.run()
 
     def robot_pose_callback(self, msg: PoseStamped):
@@ -32,14 +44,46 @@ class Reward:
         pass
 
     def new_episode_callback(self, msg: Bool):
-        with open(f'rewards/episode_{self.episode_num}.txt', 'w')as f:
-            f.write(str(self.total_reward))
-        self.episode_num += 1
-        self.total_reward = 0
+        self.new_episode = msg.data
+        if self.new_episode:
+            with open(f'{self.rewards_folder}/episode_{self.episode_num}.txt', 'w') as f:
+                f.write(str(self.total_reward))
+            self.episode_num += 1
+            self.total_reward = 0
+            self.new_episode_pub.publish(False)
+
+    def set_rewards_folder(self):
+        if not self.init_rewards_dir:
+            if rospy.has_param('/RL/runs/new_run'):
+                if rospy.get_param('/RL/runs/new_run'):
+                    c = datetime.now()
+                    current_time = c.strftime('%Y_%m_%d_%H_%M_%S')
+                    self.runs_folder = f"{self.rewards_folder}/{current_time}"
+                    os.mkdir(f"{self.runs_folder}")
+                    self.rewards_folder = f"{self.runs_folder}/rewards"
+                    os.mkdir(self.rewards_folder)
+                    self.episode_num = len(os.listdir(self.rewards_folder))
+                    self.init_rewards_dir = True
+                else:
+                    # List all folders in the directory
+                    folders = os.listdir(self.rewards_folder)
+
+                    # Filter out non-folder items and sort by name (timestamp)
+                    folders = sorted(folder for folder in folders if os.path.isdir(os.path.join(self.rewards_folder, folder)))
+                    
+                    # Select the most recent folder (the last one after sorting)
+                    if folders:
+                        self.rewards_folder = self.rospack.get_path('reinforcement_learning') + '/runs/' + folders[-1] + '/rewards/'
+                    self.episode_num = len(os.listdir(self.rewards_folder))
+                    self.init_rewards_dir = True
+                    rospy.loginfo(self.rewards_folder)
+
+            
 
     def run(self):
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
+            self.set_rewards_folder()
             rate.sleep()
 
 if __name__ == '__main__':
