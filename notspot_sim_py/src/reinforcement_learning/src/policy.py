ac2762229043a3472c6e7f83cf88c82f2eafa192
Ryan Barry <ryanbarrywheels@gmail.com>
2024-04-11 00:35:53 -0400

    Trying to fit data to network

--- notspot_sim_py/src/reinforcement_learning/src/policy.py
+++ notspot_sim_py/src/reinforcement_learning/src/policy.py
@@ -11,6 +11,7 @@ from reinforcement_learning.msg import Heuristic
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 from policy_network import PolicyNetwork
+from functools import partial
 
 
 def ros_image_to_pytorch_tensor(image_msg):
@@ -24,10 +25,6 @@ def ros_image_to_pytorch_tensor(image_msg):
     # Assuming cv_image is a numpy.uint16 array from a depth image
     # Convert the numpy array to float32
     cv_image_float = cv_image.astype(np.float32)
-    
-    # Normalize or scale the depth values if necessary
-    # For example, dividing by the maximum sensor range (in mm or meters) can bring the values to a [0, 1] range
-    # cv_image_float /= 1000.0  # Example: if the depth values are in millimeters
     
     # Convert the NumPy array (now float32) to a PyTorch tensor
     tensor_image = torch.from_numpy(cv_image_float)
@@ -48,16 +45,23 @@ class Policy:
             rospy.set_param('/RL/runs/new_run', False)
             self.model.load_state_dict(torch.load(self.hyperparameters['model_path']))
 
+        self.model.to(self.device)
+
         rospy.Subscriber('/realsense/color/image_raw', Image, self.rgb_image_callback)
         rospy.Subscriber('/realsense/depth/image_raw', Image, self.depth_image_callback)
         rospy.Subscriber('/occupancy_grid/velodyne', OccupancyGrid, self.occupancy_grid_callback)
-        rospy.Subscriber('heuristic/goal/closest', Heuristic, self.heuristic_callback)
 
-        self.rgb_image_tensor = Image()
-        self.depth_image_tensor = Image()
-        self.occupancy_grid = np.empty(100)
-        self.heuristic = Heuristic()
-        self.image_pub = rospy.Publisher("/occupancy_image", Image, queue_size=10)
+        for i in range(5):
+            rospy.Subscriber(f'heuristic/goal/{i}', Heuristic, partial(self.heuristic_callback, goal=i))
+
+
+        # self.rgb_tensor = torch.zeros(480,640,3)
+        # self.depth_tensor = torch.zeros(720,1280)
+        # self.occupancy_grid = torch.zeros(100,100)
+        self.new_data = [False, False, False, False]
+        num_goals = 5
+        self.heuristic_tensor = torch.zeros(num_goals, 3)  # Initialize with zeros
+        # self.image_pub = rospy.Publisher("/occupancy_image", Image, queue_size=10)
         self.bridge = CvBridge()
 
         self.velo_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -71,41 +75,62 @@ class Policy:
     def main_loop(self):
         while not rospy.is_shutdown():
             self.gait_pub.publish(self.gait)
+            self.predict_velocity()
 
+    def predict_velocity(self):
+        if all(self.new_data):
+            self.new_data = [False, False, False, False]
+            self.model.eval() # Set model to evaluation mode
 
+            rgb_tensor = self.rgb_tensor.unsqueeze(0).permute(0, 3, 1, 2).to(self.device)
+            depth_tensor = self.depth_tensor.to(self.device)
+            occupancy_grid_tensor = self.occupancy_grid.to(self.device)
+            heuristic_tensor = self.heuristic_tensor.to(self.device)
+
+            with torch.no_grad():
+                pred = self.model(rgb_tensor, depth_tensor, occupancy_grid_tensor, heuristic_tensor).to('cpu').squeeze().tolist()
+
+            velo = Twist()
+            velo.linear.x = pred[0]
+            velo.linear.y = pred[1]
+            velo.angular.z = pred[2]
+            # rospy.loginfo(f"Predicted Velocity: {velo}")
+            return velo
 
     def rgb_image_callback(self, img: Image):
         # Tensor of size ([480, 640, 3])
-        self.rgb_image_tensor = ros_image_to_pytorch_tensor(img)
-        rospy.loginfo(f"image tensor of size {self.rgb_image_tensor.size()}")
+        self.new_data[0] = True
+        self.rgb_tensor = ros_image_to_pytorch_tensor(img)
         
     def depth_image_callback(self, img: Image):
         # Tensor of size ([720, 1280])
-        self.depth_image_tensor = ros_image_to_pytorch_tensor(img)
-        rospy.loginfo(f"depth image tensor of size {self.depth_image_tensor.size()}")
+        self.new_data[1] = True
+        self.depth_tensor = ros_image_to_pytorch_tensor(img)
 
     def occupancy_grid_callback(self, occupancy_grid: OccupancyGrid):
-        occupancy_grid_np = np.array(occupancy_grid.data).reshape((100, 100))
+        self.new_data[2] = True
+        # occupancy_grid_np = np.array(occupancy_grid.data).reshape((100, 100))
         
-        # Map values: 100 to 0 (black) and -1 to 255 (white)
-        # First, normalize -1 to 1, then invert (1 to 0, 0 to 1), finally scale to 255
-        image_np = np.interp(occupancy_grid_np, [-1, 100], [1, 0])
-        image_np = (image_np * 255).astype(np.uint8)
+        # # Map values: 100 to 0 (black) and -1 to 255 (white)
+        # # First, normalize -1 to 1, then invert (1 to 0, 0 to 1), finally scale to 255
+        # image_np = np.interp(occupancy_grid_np, [-1, 100], [1, 0])
+        # image_np = (image_np * 255).astype(np.uint8)
         
-        # Convert numpy array to ROS Image message
-        image_msg = self.bridge.cv2_to_imgmsg(image_np, encoding="mono8")
+        # # Convert numpy array to ROS Image message
+        # image_msg = self.bridge.cv2_to_imgmsg(image_np, encoding="mono8")
         
-        # Publish the image
-        self.image_pub.publish(image_msg)
-        self.occupancy_grid = np.array(occupancy_grid.data).reshape((100,100))
+        # # Publish the image
+        # self.image_pub.publish(image_msg)
+        grid = np.array(occupancy_grid.data).reshape((100,100))
 
         # Tensor of size ([100, 100])
-        self.occupancy_grid = torch.from_numpy(self.occupancy_grid).float()
-        rospy.loginfo(f"occupancy grid tensor of size {self.occupancy_grid.size()}")
+        self.occupancy_grid = torch.from_numpy(grid).float()
+        # rospy.loginfo(f"occupancy grid tensor of size {self.occupancy_grid.size()}")
 
 
-    def heuristic_callback(self, msg: Heuristic):
-        self.heuristic = msg
+    def heuristic_callback(self, msg, goal):
+        self.new_data[3] = True
+        self.heuristic_tensor[goal] = torch.tensor([msg.x_distance, msg.y_distance, msg.manhattan_distance])
 
     
 
--- notspot_sim_py/src/reinforcement_learning/src/policy_network.py
+++ notspot_sim_py/src/reinforcement_learning/src/policy_network.py
@@ -1,23 +1,26 @@
+#!/usr/bin/env python3
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import rospy
 
 
 class CNNBranch(nn.Module):
-    def __init__(self, in_channels, output_size):
+    def __init__(self, in_channels, scaler, output_size):
         super(CNNBranch, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc = nn.Linear(64, output_size)  # Adjust output_size based on feature requirement
+        self.fc = nn.Linear(int(scaler*64), output_size)  # Adjust output_size based on feature requirement
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
         # Flatten
-        x = x.view(x.size(0), -1)
+        x = x.reshape(x.size(0), -1)
         x = F.relu(self.fc(x))
         return x
 
@@ -25,27 +28,52 @@ class PolicyNetwork(nn.Module):
     def __init__(self):
         super(PolicyNetwork, self).__init__()
         # Initialize CNN branches with appropriate input channels
-        self.rgb_branch = CNNBranch(in_channels=3, output_size=64)  # RGB image
-        self.depth_branch = CNNBranch(in_channels=1, output_size=64)  # Depth image
-        self.grid_branch = CNNBranch(in_channels=1, output_size=64)  # Grid image
+        out_sz = 64
+        self.rgb_branch = CNNBranch(in_channels=3, scaler=4800, output_size=out_sz)  # RGB image
+        self.depth_branch = CNNBranch(in_channels=1, scaler=14400, output_size=out_sz)  # Depth image
+        self.grid_branch = CNNBranch(in_channels=1, scaler=2.25, output_size=out_sz)  # Grid image
         
         # Dense network
-        self.fc1 = nn.Linear(64 * 3, 128)
+        num_goals = 5
+        self.fc1 = nn.Linear(out_sz * 3 + num_goals * 3, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 3)  # Output layer with 3 outputs
 
-    def forward(self, rgb, depth, grid):
-        # Process inputs through their respective branches
+
+    def forward(self, rgb, depth, grid, heuristic):
+        # Assuming inputs are single instances, ensure they're correctly batched
+        # No need to unsqueeze if inputs already have a batch dimension
         rgb_out = self.rgb_branch(rgb)
-        depth_out = self.depth_branch(depth)
-        grid_out = self.grid_branch(grid)
+        depth_out = self.depth_branch(depth.unsqueeze(0).unsqueeze(0))
+        grid_out = self.grid_branch(grid.unsqueeze(0))
         
-        # Concatenate outputs
-        combined = torch.cat((rgb_out, depth_out, grid_out), dim=1)
+        # Ensure 'heuristic' is correctly shaped: [1, num_goals * 3]
+        # Adjust as necessary to match your specific input shape requirements
+        heuristic_flattened = heuristic.view(1, -1)
         
-        # Dense network
+        # Concatenate outputs: Ensure dimensions align for concatenation
+        combined = torch.cat((rgb_out, depth_out, grid_out, heuristic_flattened), dim=1)
+        
+        # Dense network processing
         x = F.relu(self.fc1(combined))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-
+        rospy.loginfo(f"{x.size()}")
+        
         return x
+    # def forward(self, rgb, depth, grid, heuristic):
+    #     # Process inputs through their respective branches
+    #     depth_out = self.depth_branch(depth.unsqueeze(0))
+    #     rgb_out = self.rgb_branch(rgb).expand_as(depth_out)
+    #     grid_out = self.grid_branch(grid.unsqueeze(0))
+    #     heuristic_flattened = heuristic.view(-1).unsqueeze(0).repeat(64, 1)
+        
+    #     # Concatenate outputs
+    #     combined = torch.cat((rgb_out, depth_out, grid_out, heuristic_flattened), dim=1)
+        
+    #     # Dense network
+    #     x = F.relu(self.fc1(combined))
+    #     x = F.relu(self.fc2(x))
+    #     x = self.fc3(x)
+
+    #     return x
